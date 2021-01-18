d9d209509590e524b645931c0c3641608b47f5c0
Cristian Ariza <cristianariza89@gmail.com>
2021-01-18 19:25:30 -0300

    form

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -9,33 +9,10 @@ const Contact = () => {
       <div className={style.contact_container + " " + style.bd_grid}>
         <h2>Contact</h2>
         <form name="contact" method="POST" data-netlify="true">
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Role:{" "}
-              <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-              </select>
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message"></textarea>
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+          <input type="hidden" name="form-name" value="contact" />
+          <input id="standard-basic" label="name" name="name" />
+          <input id="standard-basic" label="email" name="email" />
+          <button type="submit">Send</button>
         </form>
       </div>
       <div className={style.contact_info}>
