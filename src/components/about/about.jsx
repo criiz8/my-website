076fad5d2bbfcd581244d2abe80b03992495d61e
Cristian Ariza <cristianariza89@gmail.com>
2021-03-26 19:17:25 -0300

    cards portfolio

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -4,34 +4,32 @@ import style from "./about.module.css";
 const About = () => {
   return (
     <section className={style.about} id="about">
-      <div className={style.about_container + " " + style.bd_grid}>
+      <div className={style.bd_grid}>
         <div className={style.me_container}>
-          <h2>I am a Full Stack Developer</h2>
+          <h1 className={style.about_title}>I am a Full Stack Developer</h1>
           <p>
             I studied web development at Henry's Bootcamp. My main goal is to
             bring you the best quality. I love to code things. Problem solver.
           </p>
         </div>
-        <div className={style.skills_container}>
-          <h2>Skills</h2>
+        <div>
+          <h1 className={style.about_title}>Skills</h1>
           <div className={style.skills_box}>
             <ul className={style.skills_list + " " + style.skills_first}>
-              <h3 className={style.skills_subtitle}>Front End</h3>
+              <h2 className={style.skills_subtitle}>Front End</h2>
               <li>
                 <span className={style.skills_name}>HTML</span>
                 <span className={style.skills_name}>CSS</span>
                 <span className={style.skills_name}>Javascript</span>
                 <span className={style.skills_name}>React</span>
-                <span className={style.skills_name}>React Native</span>
                 <span className={style.skills_name}>Redux</span>
               </li>
             </ul>
             <ul className={style.skills_list}>
-              <h3 className={style.skills_subtitle}>Back End</h3>
+              <h2 className={style.skills_subtitle}>Back End</h2>
               <li>
                 <span className={style.skills_name}>Node</span>
                 <span className={style.skills_name}>Express</span>
-                <span className={style.skills_name}>MongoDB</span>
                 <span className={style.skills_name}>PostgreSQL</span>
                 <span className={style.skills_name + " " + style.last}>
                   Sequelize
@@ -40,37 +38,66 @@ const About = () => {
             </ul>
           </div>
         </div>
-
         <div className={style.portfolio_container}>
-          <h2>Portfolio</h2>
+          <h1 className={style.about_title}>Portfolio</h1>
           <div className={style.portfolio_images + " " + style.bd_grid}>
-            <a
-              href="https://soyhenry.com/"
-              className={style.portfolio_link_name}
-            >
-              <div className={style.portfolio_img}>
-                <img src="/work1.png" alt="Mondo Countries" />
-                <div className={style.portfolio_link}>Mondo Countries </div>
-              </div>
-            </a>
-            <a
-              href="https://soyhenry.com/"
-              className={style.portfolio_link_name}
-            >
-              <div className={style.portfolio_img}>
-                <img src="/work1.png" alt="Mobile App" />
-                <div className={style.portfolio_link}>Ecommerce</div>
-              </div>
-            </a>
-            <a
-              href="https://mondocountries.netlify.com"
-              className={style.portfolio_link_name}
-            >
-              <div className={style.portfolio_img}>
-                <img src="/work1.png" alt="Mondo" />
-                <div className={style.portfolio_link}>Mobile App</div>
-              </div>
-            </a>{" "}
+            <div className={style.portfolio_div}>
+              {" "}
+              <a href="https://soyhenry.com">
+                <h2 className={style.title}>G4 Shirts</h2>
+              </a>{" "}
+              <a href="https://soyhenry.com">
+                {" "}
+                <img className={style.img} src="/work1.png" alt="Mondo" />
+              </a>
+              <p className={style.description}>
+                {" "}
+                Worked with 7 persons doing teamwork and presenting the project
+                to Henry's staff. · Authentication, 2FA, database management
+                with Sequelize, Express routing, password hashing with Bcrypt.
+              </p>{" "}
+              <a href="https://github.com/criiz8/mondo-front">
+                {" "}
+                <p className={style.source}>Source code</p>{" "}
+              </a>
+            </div>{" "}
+            <div className={style.portfolio_div}>
+              {" "}
+              <a href="https://mondocountries.netlify.com">
+                <h2 className={style.title}>Mondo Countries</h2>{" "}
+              </a>{" "}
+              <a href="https://mondocountries.netlify.com">
+                {" "}
+                <img className={style.img} src="/work1.png" alt="Mondo" />
+              </a>
+              <p className={style.description}>
+                Controlled forms, database management with PostgreSQL, React and
+                Redux states, complex searches, optimized queries, photos
+                management. Deployed with Heroku and Netlify.
+              </p>{" "}
+              <a href="https://github.com/criiz8/mondo-front">
+                {" "}
+                <p className={style.source}>Source code</p>{" "}
+              </a>
+            </div>
+            <div className={style.portfolio_div}>
+              {" "}
+              <a href="https://mondocountries.netlify.com">
+                <h2 className={style.title}>My Website</h2>{" "}
+              </a>{" "}
+              <a href="https://cristianariza.tech">
+                {" "}
+                <img className={style.img} src="/work1.png" alt="Mondo" />
+              </a>
+              <p className={style.description}>
+                Responsive react app to show my portfolio, contact form, skills
+                and who I am.
+              </p>{" "}
+              <a href="https://github.com/criiz8/cristianariza">
+                {" "}
+                <p className={style.source}>Source code</p>{" "}
+              </a>
+            </div>
           </div>
         </div>
       </div>
